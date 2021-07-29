6344c91b7ba94e7ce9a3e16f7783975a5498e018
Raskolnikov <jerry.tann@opteyes.cn>
2021-07-29 08:29:50 +0800

    feat: add gitignore file

--- tech/java/Simple.java
+++ tech/java/Simple.java
@@ -1,5 +1,14 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+class Simple {
+    public static void main(String args[]) {
+        System.out.println("Hello Java");
+        System.out.println("add 2, 3");
+        AddTest at = new AddTest();
+        System.out.println(at.add(2, 3));
+    }
+}
+
+class AddTest {
+    protected int add(int a, int b) {
+        return a + b;
+    }
+}
